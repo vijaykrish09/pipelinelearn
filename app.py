f14f5718410f9449ea97427aca98657fe51a4722
gvk09 <gvkrish912@gmail.com>
2026-04-02 18:06:30 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from http.server import SimpleHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-port = 8000
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write("<h1>Hello Vijay 🚀 DevOps App Running!</h1>".encode())
 
-server = HTTPServer(("0.0.0.0", port), SimpleHTTPRequestHandler)
-print(f"Server running on port {port}")
+server = HTTPServer(("0.0.0.0", 8000), Handler)
+print("Server running on port 8000")
 server.serve_forever()
